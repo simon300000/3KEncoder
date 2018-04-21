1277dfa0559cd55726f28e973e01697e1925bf31
simon3000 <simon3000@163.com>
2018-04-22 03:05:18 +0800

    able to resolve speech

--- src/index.js
+++ src/index.js
@@ -2,15 +2,38 @@ const trimTrailingLines = require('trim-trailing-lines')
 const splitLines = require('split-lines')
 
 const resolve = (story) => {
-  let result = []
+  let result = {}
+  let storyLine = [0]
   for (let i = 0; i < story.length; i++) {
-    console.log(story[i])
+    let chapterName = `_${storyLine.join(`_`)}`
+    if (result[chapterName] === undefined) {
+      result[chapterName] = [{
+        mark: chapterName
+      }]
+    }
+    let line = story[i]
+    if (line[0] !== `>`) {
+      if (line[0] === `[`) {
+        line = line.replace(`[`, ``)
+        line = line.split(`]`)
+        let obj = {}
+        obj[line[0]] = line[1]
+        result[chapterName].push(obj)
+      } else {
+        line = line.split(`:`)
+        result[chapterName].push({
+          speech: line
+        })
+      }
+    } else {
+      
+    }
   }
   return result
 }
 
 module.exports = (string) => {
-  if (typeof string !== 'string') {
+  if (typeof string !== `string`) {
     string = String(string)
   }
   string = trimTrailingLines(string)
