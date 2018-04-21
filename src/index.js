412242bf95391d96e7e61a529b5e51f8ab3f3d05
simon3000 <simon3000@163.com>
2018-04-22 01:34:59 +0800

    Revert "able to remove all the blank lines"
    This reverts commit f53499e3656ab7e81664b9108a5923385a8b4c1c.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
-const replaceString = require('replace-string')
-
 module.exports = (string) => {
   if (typeof string !== 'string') {
     string = String(string)
   }
-  string = replaceString(string, '\n\n', '\n')
   console.log(string)
 }
