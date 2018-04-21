db7521b7b87d4e4698d034dcff03b0e42f877837
simon3000 <simon3000@163.com>
2018-04-22 02:39:25 +0800

    able to list the story

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 const trimTrailingLines = require('trim-trailing-lines')
 const splitLines = require('split-lines')
 
+const resolve = (story) => {
+  let result = []
+  for (let i = 0; i < story.length; i++) {
+    console.log(story[i])
+  }
+  return result
+}
+
 module.exports = (string) => {
   if (typeof string !== 'string') {
     string = String(string)
@@ -12,5 +20,5 @@ module.exports = (string) => {
   for (let i = 0; i < lines; i++) {
     story.push(input[i * 2])
   }
-  console.log(story)
+  console.log(resolve(story))
 }
