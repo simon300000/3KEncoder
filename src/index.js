93de730c2e7d46d31d5e602bb1f4774da4f297c9
simon3000 <simon3000@163.com>
2018-04-22 01:41:30 +0800

    Remove new line on the end

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
+const trimTrailingLines = require('trim-trailing-lines')
+
+
 module.exports = (string) => {
   if (typeof string !== 'string') {
     string = String(string)
   }
+  string = trimTrailingLines(string)
   console.log(string)
 }
