81f60f7d62a7a97f735f37e607df2d926a58d7a5
simon3000 <simon3000@163.com>
2018-04-22 01:42:59 +0800

    cut lines

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const trimTrailingLines = require('trim-trailing-lines')
-
+const splitLines = require('split-lines')
 
 module.exports = (string) => {
   if (typeof string !== 'string') {
     string = String(string)
   }
   string = trimTrailingLines(string)
-  console.log(string)
+  let input = splitLines(string)
+  console.log(input)
 }
