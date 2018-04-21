f53499e3656ab7e81664b9108a5923385a8b4c1c
simon3000 <simon3000@163.com>
2018-04-22 01:33:25 +0800

    able to remove all the blank lines

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+const replaceString = require('replace-string')
+
 module.exports = (string) => {
   if (typeof string !== 'string') {
     string = String(string)
   }
+  string = replaceString(string, '\n\n', '\n')
   console.log(string)
 }
