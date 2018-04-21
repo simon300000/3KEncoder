0322d92b03b963138378934690eaf843f3548bdd
simon3000 <simon3000@163.com>
2018-04-22 04:57:07 +0800

    choice?

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const trimTrailingLines = require('trim-trailing-lines')
 const splitLines = require('split-lines')
+const stringOccurrence = require('string-occurrence')
+const isOdd = require('is-odd')
 
 const resolve = (story) => {
   let result = {}
   let storyLine = [0]
+  let choice = []
   for (let i = 0; i < story.length; i++) {
     let chapterName = `_${storyLine.join(`_`)}`
     if (result[chapterName] === undefined) {
@@ -13,6 +16,9 @@ const resolve = (story) => {
     }
     let line = story[i]
     if (line[0] !== `>`) {
+      if (storyLine.length > 1) {
+        storyLine = [storyLine[0] + 1]
+      }
       if (line[0] === `[`) {
         line = line.replace(`[`, ``)
         line = line.split(`]`)
@@ -26,7 +32,27 @@ const resolve = (story) => {
         })
       }
     } else {
-      
+      if (isOdd(stringOccurrence(line, `> `))) {
+        let choiceName = line.replace(`[if]`, ``)
+        while (stringOccurrence(choiceName, `> `)) {
+          choiceName = choiceName.replace(`> `, ``)
+        }
+        if (storyLine[(stringOccurrence(line, `> `) + 1) / 2] === undefined) {
+          choice.push(chapterName)
+          storyLine[(stringOccurrence(line, `> `) + 1) / 2] = 0
+          result[chapterName].push({
+            choice: {}
+          })
+        } else {
+          storyLine[(stringOccurrence(line, `> `) + 1) / 2]++
+        }
+        result[choice[storyLine.length - 2]][result[choice[storyLine.length - 2]].length - 1].choice[choiceName] = `_${storyLine.join(`_`)}`
+        // if (storyLine[stringOccurrence(line, `> `) + 1] !== undefined) {
+        //   storyLine.pop()
+        // }
+      } else {
+
+      }
     }
   }
   return result
